62e14d6dbd2cfbf127b4b7e52dd9deeaca445a7a
sara-mountassir <mountassirsara99@gmail.com>
2025-05-24 15:15:38 +0100

    Update app to use local login.html file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,5 @@
 import streamlit as st
-import requests
-
-# Django app URL configuration
-DJANGO_BASE_URL = "https://gestion-candidatures.onrender.com"
-DJANGO_LOGIN_PATHS = ["/login/", "/accounts/login/", "/admin/login/"]
-
-# Function to test URL availability
-def test_url(url):
-    try:
-        response = requests.head(url, timeout=5)
-        return response.status_code == 200
-    except:
-        return False
-
-# Find working login URL
-working_login_url = None
-for path in DJANGO_LOGIN_PATHS:
-    url = DJANGO_BASE_URL + path
-    if test_url(url):
-        working_login_url = url
-        break
+import os
 
 # Set page config
 st.set_page_config(
@@ -41,20 +21,13 @@ with col2:
     # Add some space
     st.write("")
     
-    if working_login_url:
-        # Login link - using working Django app URL
-        st.markdown(
-            f"<div style='text-align: center;'><h2>"
-            f"<a href='{working_login_url}' style='text-decoration: none; color: #FF4B4B;'>"
-            f"Se connecter 🔓</a></h2></div>",
-            unsafe_allow_html=True
-        )
-    else:
-        # Show maintenance message if Django app is not available
-        st.error("""
-        🛰️ Le système de connexion est temporairement indisponible. 
-        Veuillez réessayer dans quelques instants.
-        """)
+    # Login link - using local login.html file
+    st.markdown(
+        "<div style='text-align: center;'><h2>"
+        "<a href='candidatures/templates/candidatures/login.html' style='text-decoration: none; color: #FF4B4B;'>"
+        "Se connecter 🔓</a></h2></div>",
+        unsafe_allow_html=True
+    )
     
     # Footer
     st.markdown("""
