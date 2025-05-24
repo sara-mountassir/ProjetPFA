679f71b2fa7d2b322fd0de207a9189535c56257f
sara-mountassir <mountassirsara99@gmail.com>
2025-05-24 12:42:00 +0100

    Update settings.py

--- GestionCandidatures/settings.py
+++ GestionCandidatures/settings.py
@@ -142,9 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = '/home/sarakojima/ProjetPFA/staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Production collection
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = ['/home/sarakojima/ProjetPFA/static']
 
 
 # Default primary key field type
