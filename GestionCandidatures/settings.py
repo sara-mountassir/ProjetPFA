c017487f8fbf58186c2253546ad4c07575b45427
sara-mountassir <mountassirsara99@gmail.com>
2025-05-24 10:16:09 +0100

    Update settings.py

--- GestionCandidatures/settings.py
+++ GestionCandidatures/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'GestionCandidatures.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gestion',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Creates a file-based database
     }
 }
 
