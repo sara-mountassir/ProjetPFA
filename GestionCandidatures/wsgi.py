b160e4ba1c0d8b259559f3a71e5c5c5a3eaee91f
sara-mountassir <mountassirsara99@gmail.com>
2025-05-24 05:21:56 +0100

    Update wsgi.py

--- GestionCandidatures/wsgi.py
+++ GestionCandidatures/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for GestionCandidatures project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+path = '/home/sarakojima/ProjetPFA'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'GestionCandidatures.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GestionCandidatures.settings')
-
 application = get_wsgi_application()
