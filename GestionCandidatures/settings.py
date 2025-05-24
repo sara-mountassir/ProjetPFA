cdca569623c06475e4ecdd5ade2cbd07afe26280
sara-mountassir <mountassirsara99@gmail.com>
2025-05-24 12:15:52 +0100

    Update settings.py

--- GestionCandidatures/settings.py
+++ GestionCandidatures/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = '6&8v!3mz$4wqy9l7b_5k#p9sx@r2j+hg%f1d*o-t(cu0n)e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'sarakojima.pythonanywhere.com',
+    'www.sarakojima.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
